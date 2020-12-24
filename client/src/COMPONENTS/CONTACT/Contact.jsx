1b14c7ff9a4e552cff00afe296fd0d066e49ef9a
softwareshean <csheanjohnson@gmail.com>
2020-12-24 11:18:59 -0500

    updated syling for forms, updated component tests, added copy for about component

---
+++ client/src/COMPONENTS/CONTACT/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Contact.css';
+
+function Contact() {
+    return (
+        <div className="requestForm">
+            <h4>Have a favorite artist you'd like to add to the database?</h4>
+            <form name="contact" method="POST" data-netlify="true">
+                <input type="email" placeholder="First and Last Name" autoFocus required />
+                <textarea type="text" placeholder="Type your message here..." className="message" required/>
+                <button>SUBMIT</button>
+            </form>
+        </div>
+    );
+};
+
+export default Contact;
--- client/src/COMPONENTS/NAVIGATION/NavBar.jsx
+++ client/src/COMPONENTS/NAVIGATION/NavBar.jsx
@@ -6,9 +6,10 @@ import './NavBar.css';
 function NavBar() {
     return (
         <nav>
-            <Link to="/"><h1>CULTCATALOGUE</h1></Link>
+            <Link to="/"><h1>CULTCATALOGUE<br />API</h1></Link>
             <Link to="/about"><h2>ABOUT</h2></Link>
-            <Link to="/example"><h2>SAMPLE JSON</h2></Link>
+            <Link to="/endpoints"><h2>ENDPOINTS</h2></Link>
+            <Link to="/sample"><h2>SAMPLE JSON</h2></Link>
             <Link to="/add-form"><h2>ADD AN ARTIST</h2></Link>
             <Link to="/contact"><h2>CONTACT</h2></Link>
         </nav>
