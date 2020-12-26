067a448d06be95aa7f843618f312bb3140f413a1
softwareshean <csheanjohnson@gmail.com>
2020-12-26 15:02:25 -0500

    removed src files to fix naming convention issues on remote repo

--- client/src/COMPONENTS/ABOUT/About.test.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { mount } from 'enzyme';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
--- client/src/Hooks/useSubmit.js
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-export function useSubmit(initialName) {
-    const [name, setName] = useState();
-    return [
-        { name, onChange: (e) => setName(e.target.value) },
-        () => setName(initialName)
-    ];
-}
